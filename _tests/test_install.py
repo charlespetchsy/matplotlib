7aa77cb50bc57802646df31f5a689b1429cc49fd
Charles Petchsy <petchsycp@gmail.com>
2018-03-11 22:58:27 -0400

    testing built

--- _tests/test_install.py
+++ _tests/test_install.py
@@ -1,14 +1,25 @@
+import sys, matplotlib
 import unittest
 import matplotlib.pyplot as plt
-import sys
 
 
+# Run tests with tests.py
 class TestInstallation(unittest.TestCase):
 
     # should show "This is a test to show installation worked" in the console
     # due to a change in line 1517 in _axes.py
     def test_plot(self):
         plt.plot([1, 2, 3, 4])
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+    # Note: Backeend has already been set
+    # If running on Ubuntu -> TKAgg
 
 
 if __name__ == '__main__':
