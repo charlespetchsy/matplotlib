5cd1308323784eed27e51d8cf608e2ff287b29db
Bo Dan <maxred@5532.me>
2018-03-16 05:32:19 -0400

    fixed pep8

--- lib/matplotlib/tests/test_tricontour_segfaults.py
+++ lib/matplotlib/tests/test_tricontour_segfaults.py
@@ -3,33 +3,38 @@ import matplotlib.pyplot as plt
 import pytest
 
 
-def errFunc1():
+def err_func1():
     x, y = np.meshgrid(np.arange(10), np.arange(10))
     z = x ** 2. + y ** 2.
     z[x < y] = np.nan
     fig, ax = plt.subplots()
     ax.tricontourf(x.ravel(), y.ravel(), z.ravel())
 
-def errFunc2():
+
+def err_func2():
     x, y = np.meshgrid(np.arange(10), np.arange(10))
     z = x ** 2. + y ** 2.
     z[x < y] = np.inf
     fig, ax = plt.subplots()
     ax.tricontourf(x.ravel(), y.ravel(), z.ravel())
-    
+
+
 def func3():
     x, y = np.meshgrid(np.arange(10), np.arange(10))
     z = x ** 2. + y ** 2.
     fig, ax = plt.subplots()
     ax.tricontourf(x.ravel(), y.ravel(), z.ravel())
 
+
 def test_contour_nan():
     with pytest.raises(ValueError):
-        errFunc1()
+        err_func1()
+
 
 def test_contour_inf():
     with pytest.raises(ValueError):
-        errFunc2()
-        
+        err_func2()
+
+
 def test_contour_no_inf_or_nan():
     func3()
