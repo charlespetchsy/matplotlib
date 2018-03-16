0429e2a9fbdb0a228bea721c0a8c7104cee78e06
Charles <petchsycp@gmail.com>
2018-03-16 05:02:25 -0400

    changed to pytest

--- lib/matplotlib/tests/test_canvas_size.py
+++ lib/matplotlib/tests/test_canvas_size.py
@@ -1,126 +1,120 @@
-import sys, matplotlib
-import unittest
+import pytest
 import matplotlib.pyplot as plt
 import numpy as np
 
-class TestInstallation(unittest.TestCase):
-    # ========== Test for height changes ====================
+# ========== Test for height changes ====================
 
-    def test_canvas_size_square1(self):
-        fig1 = plt.figure(figsize=(5, 5))
-        fig2 = plt.figure(figsize=(1, 5.5))
+def test_canvas_size_square1():
+    fig1 = plt.figure(figsize=(5, 5))
+    fig2 = plt.figure(figsize=(1, 5.5))
 
-        # First figure
-        new_size = 1.1 * fig1.get_size_inches()
-        fig1.set_size_inches(new_size)
-        size1 = list(fig1.get_size_inches())
+    # First figure
+    new_size = 1.1 * fig1.get_size_inches()
+    fig1.set_size_inches(new_size)
+    size1 = list(fig1.get_size_inches())
 
-        # Second figure
-        new_size1 = fig2.get_size_inches()
-        fig2.set_size_inches(new_size1)
-        size2 = list(fig2.get_size_inches())
-        
-        self.assertEqual(size1[1], size2[1])
+    # Second figure
+    new_size1 = fig2.get_size_inches()
+    fig2.set_size_inches(new_size1)
+    size2 = list(fig2.get_size_inches())
 
+    assert(size1[1] == size2[1])
 
-    def test_canvas_size2_sqaure2(self):
-        fig1 = plt.figure(figsize=(5, 5))
-        fig2 = plt.figure(figsize=(1, 5.525))
+def test_canvas_size2_sqaure2():
+    fig1 = plt.figure(figsize=(5, 5))
+    fig2 = plt.figure(figsize=(1, 5.525))
 
-        # First figure
-        new_size = 1.105 * fig1.get_size_inches()
-        fig1.set_size_inches(new_size)
-        size1 = list(fig1.get_size_inches())
+    # First figure
+    new_size = 1.105 * fig1.get_size_inches()
+    fig1.set_size_inches(new_size)
+    size1 = list(fig1.get_size_inches())
 
-        # Second figure
-        new_size1 = fig2.get_size_inches()
-        fig2.set_size_inches(new_size1)
-        size2 = list(fig2.get_size_inches())
-        
-        self.assertEqual(size1[1], size2[1])
+    # Second figure
+    new_size1 = fig2.get_size_inches()
+    fig2.set_size_inches(new_size1)
+    size2 = list(fig2.get_size_inches())
 
-    def test_canvas_size2_rect1(self):
-        fig1 = plt.figure(figsize=(5, 3))
-        fig2 = plt.figure(figsize=(1, 3.315))
+    assert(size1[1] == size2[1])
 
-        # First figure
-        new_size = 1.105 * fig1.get_size_inches()
-        fig1.set_size_inches(new_size)
-        size1 = list(fig1.get_size_inches())
+def test_canvas_size2_rect1():
+    fig1 = plt.figure(figsize=(5, 3))
+    fig2 = plt.figure(figsize=(1, 3.315))
 
-        # Second figure
-        new_size1 = fig2.get_size_inches()
-        fig2.set_size_inches(new_size1)
-        size2 = list(fig2.get_size_inches())
-        
-        self.assertEqual(size1[1], size2[1])
+    # First figure
+    new_size = 1.105 * fig1.get_size_inches()
+    fig1.set_size_inches(new_size)
+    size1 = list(fig1.get_size_inches())
 
-    def test_canvas_size2_rect2(self):
-        fig1 = plt.figure(figsize=(8, 6))
-        fig2 = plt.figure(figsize=(1, 6.63))
+    # Second figure
+    new_size1 = fig2.get_size_inches()
+    fig2.set_size_inches(new_size1)
+    size2 = list(fig2.get_size_inches())
 
-        # First figure
-        new_size = 1.105 * fig1.get_size_inches()
-        fig1.set_size_inches(new_size)
-        size1 = list(fig1.get_size_inches())
+    assert(size1[1] == size2[1])
 
-        # Second figure
-        new_size1 = fig2.get_size_inches()
-        fig2.set_size_inches(new_size1)
-        size2 = list(fig2.get_size_inches())
-        
-        self.assertEqual(size1[1], size2[1])
+def test_canvas_size2_rect2():
+    fig1 = plt.figure(figsize=(8, 6))
+    fig2 = plt.figure(figsize=(1, 6.63))
 
+    # First figure
+    new_size = 1.105 * fig1.get_size_inches()
+    fig1.set_size_inches(new_size)
+    size1 = list(fig1.get_size_inches())
 
-    # ========== Test for proportion ====================
+    # Second figure
+    new_size1 = fig2.get_size_inches()
+    fig2.set_size_inches(new_size1)
+    size2 = list(fig2.get_size_inches())
 
-    def test_canvas_size_prop1(self):
-        fig1 = plt.figure(figsize=(8, 6))
-        fig2 = plt.figure(figsize=(8.8, 6.6))
+    assert(size1[1] == size2[1])
 
-        # First figure
-        new_size = 1.1 * fig1.get_size_inches()
-        fig1.set_size_inches(new_size)
-        size1 = list(fig1.get_size_inches())
+# ========== Test for proportion ====================
 
-        # Second figure
-        new_size1 = fig2.get_size_inches()
-        fig2.set_size_inches(new_size1)
-        size2 = list(fig2.get_size_inches())
+def test_canvas_size_prop1():
+    fig1 = plt.figure(figsize=(8, 6))
+    fig2 = plt.figure(figsize=(8.8, 6.6))
 
-        self.assertEqual(np.round(sum(size1), 5), sum(size2))
+    # First figure
+    new_size = 1.1 * fig1.get_size_inches()
+    fig1.set_size_inches(new_size)
+    size1 = list(fig1.get_size_inches())
 
-    def test_canvas_size_prop2(self):
-        fig1 = plt.figure(figsize=(5, 5))
-        fig2 = plt.figure(figsize=(6.16, 6.16))
+    # Second figure
+    new_size1 = fig2.get_size_inches()
+    fig2.set_size_inches(new_size1)
+    size2 = list(fig2.get_size_inches())
 
-        # First figure
-        new_size = 1.232 * fig1.get_size_inches()
-        fig1.set_size_inches(new_size)
-        size1 = list(fig1.get_size_inches())
+    assert(np.round(sum(size1), 5) == sum(size2))
 
-        # Second figure
-        new_size1 = fig2.get_size_inches()
-        fig2.set_size_inches(new_size1)
-        size2 = list(fig2.get_size_inches())
-        
-        self.assertEqual(sum(size1), sum(size2))
+def test_canvas_size_prop2():
+    fig1 = plt.figure(figsize=(5, 5))
+    fig2 = plt.figure(figsize=(6.16, 6.16))
 
-    def test_canvas_size_prop3(self):
-        fig1 = plt.figure(figsize=(2, 8))
-        fig2 = plt.figure(figsize=(2.2, 8.8))
+    # First figure
+    new_size = 1.232 * fig1.get_size_inches()
+    fig1.set_size_inches(new_size)
+    size1 = list(fig1.get_size_inches())
 
-        # First figure
-        new_size = 1.1 * fig1.get_size_inches()
-        fig1.set_size_inches(new_size)
-        size1 = list(fig1.get_size_inches())
+    # Second figure
+    new_size1 = fig2.get_size_inches()
+    fig2.set_size_inches(new_size1)
+    size2 = list(fig2.get_size_inches())
 
-        # Second figure
-        new_size1 = fig2.get_size_inches()
-        fig2.set_size_inches(new_size1)
-        size2 = list(fig2.get_size_inches())
-        
-        self.assertEqual(sum(size1), sum(size2))
+    assert(sum(size1) == sum(size2))
 
-if __name__ == '__main__':
-    unittest.main()
+def test_canvas_size_prop3():
+    fig1 = plt.figure(figsize=(2, 8))
+    fig2 = plt.figure(figsize=(2.2, 8.8))
+
+    # First figure
+    new_size = 1.1 * fig1.get_size_inches()
+    fig1.set_size_inches(new_size)
+    size1 = list(fig1.get_size_inches())
+
+    # Second figure
+    new_size1 = fig2.get_size_inches()
+    fig2.set_size_inches(new_size1)
+    size2 = list(fig2.get_size_inches())
+
+    assert(sum(size1) == sum(size2))
+
