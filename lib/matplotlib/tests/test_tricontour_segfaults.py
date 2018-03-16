9ee611b9cac9a5c8c16829ce5c09ac2ce9f7285f
Christophe <christopheperrin4@yahoo.com>
2018-03-16 02:48:02 -0400

    added test case

--- lib/matplotlib/tests/test_tricontour_segfaults.py
+++ lib/matplotlib/tests/test_tricontour_segfaults.py
@@ -16,7 +16,12 @@ def errFunc2():
     z[x < y] = np.inf
     fig, ax = plt.subplots()
     ax.tricontourf(x.ravel(), y.ravel(), z.ravel())
-
+    
+def func3():
+    x, y = np.meshgrid(np.arange(10), np.arange(10))
+    z = x ** 2. + y ** 2.
+    fig, ax = plt.subplots()
+    ax.tricontourf(x.ravel(), y.ravel(), z.ravel())
 
 def test_contour_nan():
     with pytest.raises(ValueError):
@@ -25,3 +30,6 @@ def test_contour_nan():
 def test_contour_inf():
     with pytest.raises(ValueError):
         errFunc2()
+        
+def test_contour_no_inf_or_nan():
+    func3()
